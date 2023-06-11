218ae403858de0d0fd359d5a3827c6f1b971ccb5
Severin Ibarluzea <seveibar@gmail.com>
2023-06-11 14:40:31 -0700

    ready for publish

---
+++ README.md
@@ -0,0 +1,3 @@
+# log-soup
+
+Simple utility for logging soup JSON to [debug.tscircuit.com](https://debug.tscircuit.com)
---
+++ src/index.ts
@@ -0,0 +1,70 @@
+import defaultAxios from "redaxios"
+
+const DEBUG_SRV = `https://debug.tscircuit.com`
+
+const axios = defaultAxios.create({
+  baseURL: DEBUG_SRV,
+})
+
+function findSource(elm: any, sources: Array<any>) {
+  if ("source_component_id" in elm) {
+    return sources.find(
+      (s) =>
+        "source_component_id" in s &&
+        s.source_component_id === elm.source_component_id &&
+        s.type === "source_component"
+    )
+  }
+  if ("source_port_id" in elm) {
+    return sources.find(
+      (s) =>
+        "source_port_id" in s &&
+        s.source_port_id === elm.source_port_id &&
+        s.type === "source_port"
+    )
+  }
+  return null
+}
+
+let layout_server_healthy: boolean | null = null
+export const logSoup = async (
+  layout_group_name: string,
+  objects: Array<any>
+) => {
+  if (layout_server_healthy === false) return
+
+  if (layout_server_healthy === null) {
+    try {
+      await axios.get("/api/health", {
+        timeout: 5000,
+      } as any)
+      layout_server_healthy = true
+    } catch (e) {
+      console.log("layout server unhealthy, not logging soup")
+      layout_server_healthy = false
+      return
+    }
+  }
+
+  for (const layout_name of ["schematic", "pcb"]) {
+    await axios.post("/api/soup_group/add_soup", {
+      soup_group_name: layout_group_name,
+      soup_name: layout_name,
+      username: "tmp",
+      content: {
+        elements: objects
+          .filter((o) => o.type?.includes(layout_name))
+          .map((o: any) => ({
+            ...o,
+            source: findSource(o, objects),
+          })),
+      },
+    })
+  }
+
+  return {
+    soup_schematic_url: `${DEBUG_SRV}/soup_group/${layout_group_name}#selected_layout_index=0&selected_engine=schematic_renderer`,
+    soup_pcb_url: `${DEBUG_SRV}/soup_group/${layout_group_name}#selected_layout_index=1&selected_engine=pcb_renderer`,
+    soup_pcb_debug_url: `${DEBUG_SRV}/soup_group/${layout_group_name}#selected_layout_index=1&selected_engine=debug_renderer`,
+  }
+}
